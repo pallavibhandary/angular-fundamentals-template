067e6fa74aa481751978938ca9ed06ff870ad657
pallavibhandary <53182343+pallavibhandary@users.noreply.github.com>
2024-03-18 23:56:38 +0530

    Update course-card.component.ts

--- src/app/shared/components/course-card/course-card.component.ts
+++ src/app/shared/components/course-card/course-card.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { mockedCoursesList } from '../../mocks/mock';
 
 @Component({
-  selector: 'app-course-card',
-  templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.scss']
+    selector: 'app-course-card',
+    templateUrl: './course-card.component.html',
+    styleUrls: ['./course-card.component.scss']
 })
-export class CourseCardComponent {}
+
+export class CourseCardComponent {
+    showCourse = 'SHOW COURSE';
+    icons: string = "['fas', 'trash']";
+    iconsEdit: string = "['fas', 'edit']";
+    mockedCoursesList1: any;
+    editable: Boolean = false;
+    @Output() clickOnShow = new EventEmitter();
+
+    constructor() {
+        this.mockedCoursesList1 = mockedCoursesList;
+    }
+
+    showCourseDetails(item: any) {
+        this.clickOnShow.emit(item);
+    }
